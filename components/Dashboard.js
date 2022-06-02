ce8c6af19a509fe80025c0b25748ebaea8b07bec
Juan Antonio V <juananthonio23@gmail.com>
2022-06-02 14:49:38 +0700

    Make Sidebar, Search Components

---
+++ components/Dashboard.js
@@ -0,0 +1,13 @@
+import Body from './Body';
+import Right from './Right';
+import Sidebar from './Sidebar';
+
+export default function Dashboard() {
+  return (
+    <main>
+      <Sidebar />
+      <Body />
+      <Right />
+    </main>
+  );
+}
